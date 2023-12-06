ec6005c29d163ace7ff6c7e5a2816ec6a954498f
luca khatri <luca2@MacBook-Pro.local>
2023-12-06 18:10:50 +0000

    installed and added blade starterkit to project for auth

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,12 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users', [UserController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/users/{id}', [UserController::class, 'show']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/feed', [PostController::class, 'index'])->name('posts.index');
-
-Route::get('/addPost', [PostController::class, 'create']);
-
-Route::post('/addPost/store', [PostController::class, 'store'])->name('posts.store');
+require __DIR__.'/auth.php';
